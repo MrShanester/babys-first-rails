8e0499f3bb7408ac7683f3f8ebc5f009669455c3
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-12 14:30:38 -0700

    Adds about path and controller

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,4 +2,8 @@ class WelcomesController < ApplicationController
   def hello_method
     render json: {message: "Hello from welcomes controller!"}
   end
+
+  def about_method
+    render json: {message: "Programming can be challenging and rewarding. With Ruby, my favorite language, programming comes more naturally as the syntax is quite intuitive."}
+  end
 end
